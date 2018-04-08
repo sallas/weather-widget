fb74fd35cc0ac6a56061d8d8e4de1d148677be95
Sallas <tbc105@hotmail.com>
2018-04-08 15:29:59 +0200

    Add CORS to backend api

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 3001;
 
 const getWeather = require('./weatherWidgetNode');
+
+app.use(cors());
 
 app.get('/', async (req, res) => res.send(await getWeather(req.query.city)));
 
